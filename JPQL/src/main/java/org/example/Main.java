13a1082e584ac92ce4e7cd596710cc8f515b9c94
Charith Miriharnga <charithmihiranga453@gmail.com>
2024-09-19 18:42:22 +0530

    jpql module done.

--- JPQL/src/main/java/org/example/Main.java
+++ JPQL/src/main/java/org/example/Main.java
@@ -1,7 +1,78 @@
 package org.example;
 
+import org.example.config.FactoryConfiguration;
+import org.example.entity.Address;
+import org.example.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+//        INSERT
+        /*Customer customer = new Customer();
+        customer.setId(2);
+        customer.setName("Kamal");
+        customer.setAddressList(new ArrayList<>());
+
+        Address address = new Address();
+        address.setaId(3);
+        address.setNo("123");
+        address.setRoad("Main Street");
+        address.setCity("Matale");
+        address.setCustomer(customer);
+
+        Address address1 = new Address();
+        address1.setaId(4);
+        address1.setNo("1234");
+        address1.setRoad("Olter salgadu road");
+        address1.setCity("Panadura");
+        address.setCustomer(customer);
+
+        customer.getAddressList().add(address);
+        customer.getAddressList().add(address1);*/
+
+
+//      DELETE
+         /*String addressToDelete = "3";
+        String jpql = "DELETE FROM Address a WHERE a.aId = :aId";
+        session.createQuery(jpql)
+                .setParameter("aId", addressToDelete)
+                .executeUpdate();*/
+
+//        UPDATE
+       /* int customerId = 1;
+        String newName = "Sumith";
+        String jpql = "UPDATE Customer c SET c.name = :newName WHERE c.id = :id";
+        session.createQuery(jpql).setParameter("newName", newName)
+                .setParameter("id", customerId)
+                .executeUpdate();*/
+
+//        SELECT ALL
+        /*String jpql = "SELECT c FROM Customer c";
+        List<Customer> customerList = session.createQuery(jpql, Customer.class).getResultList();
+
+        System.out.println("Customer List:");
+        for (Customer customer : customerList) {
+            System.out.println(customer);
+        }*/
+
+
+//        SEARCH
+        /*String searchName = "Sumith";
+        String jpql = "SELECT c FROM Customer c WHERE c.name = :name";
+        List<Customer> customers = session.createQuery(jpql, Customer.class)
+                .setParameter("name", searchName)
+                .getResultList();*/
+
+        transaction.commit();
+        session.close();
+    }
 }
---
+++ JPQL/src/main/java/org/example/entity/Customer.java
@@ -0,0 +1,56 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+//    Invers side
+@Entity
+public class Customer {
+    @Id
+    private int id;
+    private String name;
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addressList;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, List<Address> addressList) {
+        this.id = id;
+        this.name = name;
+        this.addressList = addressList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Address> getAddressList() {
+        return this.addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addressList=" + addressList +
+                '}';
+    }
+}
